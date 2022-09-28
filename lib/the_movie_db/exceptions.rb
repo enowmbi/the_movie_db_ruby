6699be622c8b1e8d1b9d3b9be198d8184a4f1fcb
Enow Mbi <benowmbi@yahoo.com>
2022-09-28 11:03:37 +0100

    Add exception class

--- lib/the_movie_db/exceptions.rb
+++ lib/the_movie_db/exceptions.rb
@@ -1,4 +1,38 @@
 # frozen_string_literal: true
 
 module TheMovieDb
+  # BadRequestException - 400
+  class BadRequestException < StandardError
+    def initialize(json)
+      puts json["status_message"] || super
+    end
+  end
+
+  # UnauthorizedException - 401
+  class UnauthorizedException < StandardError
+    def initialize(json)
+      puts json["status_message"] || super
+    end
+  end
+
+  # ForbiddenException - 403
+  class ForbiddenException < StandardError
+    def initialize(json)
+      puts json["status_message"] || super
+    end
+  end
+
+  # NotFoundException - 404
+  class NotFoundException < StandardError
+    def initialize(json)
+      puts json["status_message"] || super
+    end
+  end
+
+  # InternalServerError - 500
+  class ServerException < StandardError
+    def initialize(json)
+      puts json["status_message"] || super
+    end
+  end
 end
