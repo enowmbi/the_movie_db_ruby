5226422bf9bd4d6e987395be05578d703e7b60f8
Enow Mbi <benowmbi@yahoo.com>
2022-09-28 11:54:13 +0100

    Autoload exception classes in the_movie_db.rb

--- lib/the_movie_db.rb
+++ lib/the_movie_db.rb
@@ -4,5 +4,9 @@ require_relative "the_movie_db/version"
 
 # TheMovieDb module - autoload dependency classes
 module TheMovieDb
-  # Your code goes here...
+  autoload(:BadRequestException, "the_movie_db/exceptions")
+  autoload(:UnauthorizedException, "the_movie_db/exceptions")
+  autoload(:ForbiddenException, "the_movie_db/exceptions")
+  autoload(:NotFoundException, "the_movie_db/exceptions")
+  autoload(:ServerException, "the_movie_db/exceptions")
 end
